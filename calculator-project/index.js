b5c58f2bd3007b20d3d5b18271d96e43c816f7ed
andrew-bello <92801912+Andrew-Bello@users.noreply.github.com>
2025-09-23 14:50:51 +0100

    Number pressed logic and display input logic added

--- calculator-project/index.js
+++ calculator-project/index.js
@@ -1,7 +1,45 @@
-const nums = document.querySelectorAll(".num");
+const nums = document.querySelectorAll('.num');
+const operators = document.querySelectorAll('.operator');
+const input = document.querySelector('.input');
+const cancelBtn = document.querySelector('.cancel');
+const backspaceBtn = document.querySelector('.backspace');
 
-for(let i = 0; i < nums.length; i++) {
-    nums[i].addEventListener('click', () => {
-        console.log(nums[i].value)
-    })
+const inputValue = [];
+const numArr = [];
+let joinedValue;
+let result; 
+
+function numsPressed() {
+    for(let i = 0; i < nums.length; i++) {
+        nums[i].addEventListener('click', () => {
+            inputValue.push(nums[i].value);
+            joinedValue = Number(inputValue.join(''));
+            input.innerText = joinedValue;
+            console.log(joinedValue, typeof joinedValue)
+        })
+    }
 }
+
+numsPressed()
+
+function opsPressed() {
+    for(let i = 0; i < operators.length; i++) {
+        operators[i].addEventListener('clcik', () => {
+            console.log(operators[i].value)
+        })
+    }
+}
+
+opsPressed()
+
+const newArr = [1,3,4,5];
+
+function multiply(arr) {
+    let sum = 1
+    for(let i = 0; i < arr.length; i++) {
+        sum *= arr[i];
+    }
+    return sum;
+}
+
+console.log(multiply(newArr));
